e0ba4cb84066c8adb75bfe0da3f08583406498ff
Haci Murat Yaman <haci@muratyaman.co.uk>
2016-09-25 12:26:48 +0100

    done listing tables and columns

--- compare.py
+++ compare.py
@@ -10,62 +10,68 @@ from sqlalchemy.engine import reflection
 class MyDb:
 
     def __init__ (self):
+        print('MyDb new instance.')
         self.sshServer    = None
         self.dbEngine     = None
         self.dbConnection = None
+        self.metadata     = None
     # end constructor
 
     def connect(self, config, dbRef):
+        print('MyDb connect: reading config ...')
         defaults  = config['defaults']
         hosts     = config['hosts']
         databases = config['databases']
         users     = config['users']
 
         db = databases[dbRef]
-        dbHostRef  = db['host']
-        dbUserRef  = db['user']
-        dbName     = db['name']
-        dbHostIp   = hosts[dbHostRef]
-        dbPort     = defaults['db_port'] #if 'db_port' in defaults else 22
-        if 'port' in db:
-            dbPort = db['port']
-        # end if
-        dbUser     = users[dbHostRef]
-        dbUserName = dbUser['name']
-        dbUserPass = dbUser['password']
 
         tunnel = db['tunnel']
         sshHostRef  = tunnel['host']
         sshUserRef  = tunnel['user']
-        sshHostIp   = hosts[sshHostRef]
+        sshHost     = hosts[sshHostRef]
+        sshHostAdrs = sshHost['address']
         sshPort     = defaults['ssh_port'] #if 'ssh_port' in defaults else 3306
-        if 'port' in tunnel:
-            sshPort = tunnel['port']
+        if 'port' in sshHost:
+            sshPort = sshHost['port']
         # end if
         sshUser     = users[sshUserRef]
         sshUserName = sshUser['name']
         sshUserPass = sshUser['password']
 
+        dbHostRef  = db['host']
+        dbUserRef  = db['user']
+        dbName     = db['name']
+        dbHost     = hosts[dbHostRef]
+        dbHostAdrs = dbHost['address']
+        dbPort     = defaults['db_port'] #if 'db_port' in defaults else 22
+        if 'port' in dbHost:
+            dbPort = dbHost['port']
+        # end if
+        dbUser     = users[dbUserRef]
+        dbUserName = dbUser['name']
+        dbUserPass = dbUser['password']
+
+        print('MyDb connect: SSH connection instance ...')
         # SSH connection
         self.sshServer = SSHTunnelForwarder(
-            sshHostIp,
+            sshHostAdrs,
             ssh_username = sshUserName,
             ssh_password = sshUserPass,
-            remote_bind_address=(dbHostIp, dbPort)
+            remote_bind_address=(dbHostAdrs, dbPort)
         )
 
         print('SSH tunnel: starting ...')
         self.sshServer.start()
         print('SSH tunnel: started.')
 
-        print('SSH tunnel: ready ')
-        print(self.sshServer.local_bind_port)
+        print('SSH tunnel: ready port: ', self.sshServer.local_bind_port)
 
         # database connection
         dbType = 'mysql+mysqlconnector'
         dbPort = self.sshServer.local_bind_port
         dsn = '{}://{}:{}@{}:{}/{}'.format(
-            dbType, dbUserName, dbUserPass, dbHostIp, dbPort, dbName
+            dbType, dbUserName, dbUserPass, dbHostAdrs, dbPort, dbName
         )
 
         print('Database engine: starting ...')
@@ -73,7 +79,7 @@ class MyDb:
         print('Database engine: started.')
 
         print('Database connection: connecting ...')
-        self.dbConnection = dbEngine.connect()
+        self.dbConnection = self.dbEngine.connect()
         print('Database connection: connected.')
 
         print('Database version: querying ...')
@@ -86,17 +92,26 @@ class MyDb:
 
     def tables(self):
         print('Database metadata: loading ...')
-        metadata = MetaData()
-        metadata.reflect(bind=self.dbEngine)
+        self.metadata = MetaData()
+        self.metadata.reflect(bind=self.dbEngine)
         print('Database metadata: loaded.')
 
-        inspector = inspect(dbEngine)
-
-        for tbl in metadata.tables:
-            print('Table ', tbl)
-            columns = inspector.get_columns(tbl)
-            pprint.pprint(columns)
+        for tblName in self.metadata.sorted_tables:
+            self.table(tblName)
         # end for
+    # end function
+
+    def table(self, tableName):
+        print('Table: ', tableName, ' columns listing ...')
+        myTable = Table(tableName, self.metadata, autoload=True, autoload_with=self.dbEngine)
+        for myColumn in myTable.columns:
+            print('Table: ', tableName, ' Column: ', myColumn.name, ' Type: ', myColumn.type, 'Nullable: ', myColumn.nullable)
+        # end for
+        print('Table: ', tableName, ' columns listed.')
+    # end function
+
+    def storeTableColumnInfo(self, table, column):
+        print('Store info ...')
     # end function
 
     def close(self):
@@ -139,23 +154,25 @@ class MyApp:
         for key in compareDbs:
             sourceDbRef = key
             targetDbRef = compareDbs[key]
-            try:
+            #try:
+            if 1:
                 sourceDb = MyDb()
                 sourceDb.connect(self.config, sourceDbRef)
                 sourceTables = sourceDb.tables()
-            except Exception as err:
-                print('Source database: error: ', err)
-            else: # finally
+            #except Exception as err:
+            #    print('Source database: error: ', err)
+            #else: # finally
                 sourceDb.close()
             # end try catch
 
-            try:
+            #try:
+            if 1:
                 targetDb = MyDb()
                 targetDb.connect(self.config, targetDbRef)
                 targetTables = targetDb.tables()
-            except Exception as err:
-                print('Target database: error: ', err)
-            else: # finally
+            #except Exception as err:
+            #    print('Target database: error: ', err)
+            #else: # finally
                 targetDb.close()
             # end try catch
         # end for loop
@@ -171,15 +188,16 @@ class MyApp:
 
 def main():
 
-    try:
+    #try:
+    if 1:
         configFilePath = './config.json'
         myApp = MyApp(configFilePath)
         myApp.start()
         myApp.finish()
-    except Exception as err:
-        print('App error: ', err)
-    else: # finally
-        print('The End!')
+    #except Exception as err:
+    #    print('App error: ', err)
+    #else: # finally
+    #    print('The End!')
     # end try catch
 
 # end function main
