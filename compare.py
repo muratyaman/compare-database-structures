cd3ec50c9134b767d2d8a61a7a5c34d2e17fb689
Haci Murat Yaman <haci.yaman@aptcontrols.co.uk>
2016-09-26 12:30:04 +0100

    improvements

--- compare.py
+++ compare.py
@@ -1,4 +1,4 @@
-import sys
+import logging
 import json
 import pprint
 from sshtunnel import SSHTunnelForwarder
@@ -9,22 +9,23 @@ from sqlalchemy.engine import reflection
 
 class MyDb:
 
-    def __init__ (self):
-        print('MyDb new instance.')
+    def __init__ (self, dbRef):
+        self.dbRef        = dbRef
+        logging.debug('DB: ' + self.dbRef + ' > new instance.')
         self.sshServer    = None
         self.dbEngine     = None
         self.dbConnection = None
         self.metadata     = None
     # end constructor
-
-    def connect(self, config, dbRef):
-        print('MyDb connect: reading config ...')
+    
+    def connect(self, config):
+        logging.debug('DB: ' + self.dbRef + ' > connect: reading config ...')
         defaults  = config['defaults']
         hosts     = config['hosts']
         databases = config['databases']
         users     = config['users']
 
-        db = databases[dbRef]
+        db = databases[self.dbRef]
 
         tunnel = db['tunnel']
         sshHostRef  = tunnel['host']
@@ -52,7 +53,7 @@ class MyDb:
         dbUserName = dbUser['name']
         dbUserPass = dbUser['password']
 
-        print('MyDb connect: SSH connection instance ...')
+        logging.debug('DB: ' + self.dbRef + ' > connect: SSH connection instance ...')
         # SSH connection
         self.sshServer = SSHTunnelForwarder(
             sshHostAdrs,
@@ -61,71 +62,87 @@ class MyDb:
             remote_bind_address=(dbHostAdrs, dbPort)
         )
 
-        print('SSH tunnel: starting ...')
+        logging.debug('DB: ' + self.dbRef + ' > SSH tunnel: starting ...')
         self.sshServer.start()
-        print('SSH tunnel: started.')
+        logging.debug('DB: ' + self.dbRef + ' > SSH tunnel: started.')
 
-        print('SSH tunnel: ready port: ', self.sshServer.local_bind_port)
+        logging.debug('DB: ' + self.dbRef + ' > SSH tunnel: ready port: ' + str(self.sshServer.local_bind_port))
 
         # database connection
         dbType = 'mysql+mysqlconnector'
-        dbPort = self.sshServer.local_bind_port
+        dbHostAdrs = '127.0.0.1'                # override db host
+        dbPort = self.sshServer.local_bind_port # override db port
         dsn = '{}://{}:{}@{}:{}/{}'.format(
             dbType, dbUserName, dbUserPass, dbHostAdrs, dbPort, dbName
         )
 
-        print('Database engine: starting ...')
+        logging.debug('DB: ' + self.dbRef + ' > Database engine: starting ...')
         self.dbEngine = create_engine(dsn)
-        print('Database engine: started.')
+        logging.debug('DB: ' + self.dbRef + ' > Database engine: started.')
 
-        print('Database connection: connecting ...')
+        logging.debug('DB: ' + self.dbRef + ' > Database connection: connecting ...')
         self.dbConnection = self.dbEngine.connect()
-        print('Database connection: connected.')
+        logging.info('DB: ' + self.dbRef + ' > Database connection: connected.')
 
-        print('Database version: querying ...')
-        result = self.dbConnection.execute("SELECT version() AS version")
+        logging.debug('DB: ' + self.dbRef + ' > Database version: querying ...')
+        result = self.dbConnection.execute('SELECT version() AS version')
         for row in result:
-            version = row['version'] if 'version' in row else 'N/A'
-            print("Database version: ", version)
+            version = str(row['version']) if 'version' in row else 'N/A'
+            logging.info('Database version: {}'.format(version))
         # end for
     # end function
 
     def tables(self):
-        print('Database metadata: loading ...')
+        logging.debug('DB: ' + self.dbRef + ' > metadata: loading ...')
         self.metadata = MetaData()
         self.metadata.reflect(bind=self.dbEngine)
-        print('Database metadata: loaded.')
+        logging.debug('DB: ' + self.dbRef + ' > metadata: loaded.')
 
-        for tblName in self.metadata.sorted_tables:
-            self.table(tblName)
+        logging.debug('DB: ' + self.dbRef + ' > tables: listing ...')
+        print('')
+        print('DB: ' + self.dbRef + ' > Tables: reading ', end='')
+        for tbl in self.metadata.sorted_tables:
+            print('.', end='') # continue on same line
+            self.table(tbl)
         # end for
+        print(' done.', end='')
+        print('')
+        logging.debug('DB: ' + self.dbRef + ' > tables: listed.')
     # end function
 
-    def table(self, tableName):
-        print('Table: ', tableName, ' columns listing ...')
-        myTable = Table(tableName, self.metadata, autoload=True, autoload_with=self.dbEngine)
+    def table(self, myTable):
+        logging.debug('DB: ' + self.dbRef + ' >> Table: ' + myTable.name + ' - Columns listing ...')
+        #myTable = Table(myTable.name, self.metadata, autoload=True, autoload_with=self.dbEngine)
         for myColumn in myTable.columns:
-            print('Table: ', tableName, ' Column: ', myColumn.name, ' Type: ', myColumn.type, 'Nullable: ', myColumn.nullable)
+            logging.debug(
+                'DB: ' + self.dbRef + ' >> Table: ' + myTable.name + ' Column: ' + myColumn.name +
+                ' Type: ' + str(myColumn.type) + ' Nullable: ' + str(myColumn.nullable)
+            )
         # end for
-        print('Table: ', tableName, ' columns listed.')
+        logging.debug('DB: ' + self.dbRef + ' >> Table: ' + myTable.name + ' - Columns listed.')
     # end function
 
     def storeTableColumnInfo(self, table, column):
-        print('Store info ...')
+        logging.debug('DB: ' + self.dbRef + ' > Store info ...')
     # end function
 
     def close(self):
-        print('Database connection: closing ...')
+        logging.debug('DB: ' + self.dbRef + ' > Database connection: closing ...')
         try:
             self.dbConnection.close()
         except Exception as err:
-            print('Database connection: error on close(): ', err)
+            logging.warning('DB: ' + self.dbRef + ' > Database connection: error on close(): ')
+            print(err)
         # end try catch
-        print('Database connection: closed.')
+        logging.debug('DB: ' + self.dbRef + ' > Database connection: closed.')
 
-        print('SSH tunnel: stopping ...')
-        self.sshServer.stop()
-        print('SSH tunnel: stopped.')
+        logging.debug('DB: ' + self.dbRef + ' > SSH tunnel: stopping ...')
+        try:
+            self.sshServer.stop()
+        except Exception as err:
+            logging.warning('DB: ' + self.dbRef + ' > SSH tunnel: error on close(): ')
+            print(err)
+        logging.debug('DB: ' + self.dbRef + ' > SSH tunnel: stopped.')
     # end function
 
 # end class MyDb
@@ -133,46 +150,52 @@ class MyDb:
 class MyApp:
 
     def __init__ (self, configFilePath):
-        print('MyApp: new instance.')
+        logging.debug('MyApp: new instance.')
         self.read(configFilePath)
     # end constructor
-
+    
     def read(self, configFilePath):
         self.config = None
-        print('Config file: opening ...')
+        logging.debug('MyApp: Config file: opening ...')
         with open(configFilePath) as configFile:
-            print('Config file: opened.')
-            print('Config json: loading ...')
+            logging.debug('MyApp: Config file: opened.')
+            logging.debug('MyApp: Config json: loading ...')
             self.config = json.load(configFile)
-            print('Config json: loaded.')
+            logging.debug('MyApp: Config json: loaded.')
             # pprint.pprint(config)
         # end with
     # end function
 
     def start(self):
+        logging.debug('MyApp: starting ...')
         compareDbs = self.config['compare']
         for key in compareDbs:
             sourceDbRef = key
             targetDbRef = compareDbs[key]
-            #try:
-            if 1:
-                sourceDb = MyDb()
-                sourceDb.connect(self.config, sourceDbRef)
+            logging.debug('MyApp: comparing: ' + sourceDbRef + ' with ' + targetDbRef)
+            try:
+                logging.debug('MyApp: loading: ' + sourceDbRef)
+                sourceDb = MyDb(sourceDbRef)
+                logging.debug('MyApp: loaded: ' + sourceDbRef)
+                sourceDb.connect(self.config)
+                logging.debug('MyApp: connected: ' + sourceDbRef)
                 sourceTables = sourceDb.tables()
-            #except Exception as err:
-            #    print('Source database: error: ', err)
-            #else: # finally
+            except Exception as err:
+                print('Source database: error: ', err)
+            else: # finally
+                logging.debug('MyApp: closing db: ' + sourceDbRef)
                 sourceDb.close()
             # end try catch
 
-            #try:
-            if 1:
-                targetDb = MyDb()
-                targetDb.connect(self.config, targetDbRef)
+            try:
+                logging.debug('MyApp: loading: ' + targetDbRef)
+                targetDb = MyDb(targetDbRef)
+                targetDb.connect(self.config)
                 targetTables = targetDb.tables()
-            #except Exception as err:
-            #    print('Target database: error: ', err)
-            #else: # finally
+            except Exception as err:
+                print('Target database: error: ', err)
+            else: # finally
+                logging.debug('MyApp: closing db: ' + targetDbRef)
                 targetDb.close()
             # end try catch
         # end for loop
@@ -187,20 +210,25 @@ class MyApp:
 # END definitions ==============================================================
 
 def main():
-
-    #try:
-    if 1:
+    try:
+        print('Running ...')
+        logging.basicConfig(filename='compare.log', level=logging.DEBUG)
         configFilePath = './config.json'
+        logging.debug('App: loading...')
         myApp = MyApp(configFilePath)
+        logging.debug('App: loaded.')
+        logging.debug('App: starting...')
         myApp.start()
+        logging.debug('App: started.')
+        logging.debug('App: finishing...')
         myApp.finish()
-    #except Exception as err:
-    #    print('App error: ', err)
-    #else: # finally
-    #    print('The End!')
+        logging.debug('App: finished.')
+    except Exception as err:
+        print('App error: ', err)
+    else: # finally
+        print('The End!')
     # end try catch
-
 # end function main
 
 main()
-sys.exit(0)
+print (exit)
