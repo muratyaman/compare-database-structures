6ec82329b93746db141e3d8e2ae411e81153fe01
Haci Murat Yaman <haci.yaman@aptcontrols.co.uk>
2016-09-26 15:40:53 +0100

    done saving info to local sqlite db

--- compare.py
+++ compare.py
@@ -4,20 +4,144 @@ import pprint
 from sshtunnel import SSHTunnelForwarder
 from sqlalchemy import *
 from sqlalchemy.engine import reflection
+from sqlite3 import dbapi2 as sqlite
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
 
 # START definitions ============================================================
 
-class MyDb:
+'''
+class MyDbLocalTableColumnModel(Base):
+    __table__ = 'my_tables'
+    
+    id              = Column(Integer, primary_key=True)
+    db_ref          = Column(String)
+    schema_name     = Column(String)
+    table_name      = Column(String)
+    column_name     = Column(String)
+    column_type     = Column(String)
+    column_nullable = Column(Integer)
+    column_attrs    = Column(String)
 
-    def __init__ (self, dbRef):
+    def __repr__(self):
+        return "<MyDbLocalTableColumnModel (db_ref='%s', schema_name='%s', table_name='%s', column_name='%s')>" % (
+            self.db_ref, self.schema_name, self.table_name, self.column_name
+        )
+    # end function
+    
+# end class MyDbLocalTableColumnModel
+'''
+
+class MyDbLocal:
+    
+    def __init__ (self):
+        self.dbEngine     = None
+        self.dbConnection = None
+        self.dbCursor     = None
+    # end constructor
+    
+    def connect(self, dbFile):
+        '''
+        dbType = 'sqlite+pysqlite'
+        #dbFile = './db.sqlite3'
+        dsn = '{}:///{}'.format(
+            dbType, dbFile
+        )
+
+        logging.debug('Local database engine: starting ...')
+        self.dbEngine = create_engine(dsn, module=sqlite)
+        logging.debug('Local database engine: started.')
+
+        logging.debug('Local database connection: connecting ...')
+        self.dbConnection = self.dbEngine.connect()
+        logging.info('Local database connection: connected.')
+        
+        logging.debug('Local database: new session ...')
+        self.session = sessionmaker()
+        self.session.configure(bind=self.dbEngine)
+        logging.debug('Local database: new session ready.')
+        '''
+        
+        self.dbConnection = sqlite.connect(dbFile)
+        self.dbCursor     = self.dbConnection.cursor()
+    # end function
+    
+    def saveTableColumn(self, dbRef, schemaName, table, column):
+        '''
+        myTableCol = MyDbLocalTableColumnModel(
+            db_ref=dbRef, schema_name=schemaName, table_name=table.name, column_name=column.name,
+            column_type=str(column.type), column_nullable=1 if column.nullable else 0
+        )
+        self.session.add(myTableCol)
+        self.session.commit()
+        '''
+        
+        '''
+        insertSql = ('INSERT INTO my_tables ' +
+            '(db_ref, schema_name, table_name, column_name, column_type, column_nullable) ' +
+            'VALUES (:db_ref, :schema_name, :table_name, :column_name, :column_type, :column_nullable)'
+        )
+        params = {
+            "db_ref"          : dbRef,
+            "schema_name"     : schemaName,
+            "table_name"      : table.name,
+            "column_name"     : column.name,
+            "column_type"     : str(column.type),
+            "column_nullable" : 1 if column.nullable else 0
+        }
+        result = self.dbCursor.execute(
+            insertSql,
+            params
+        )
+        '''
+        
+        insertSql = ('INSERT INTO my_tables ' +
+            '(db_ref, schema_name, table_name, column_name, column_type, column_nullable) ' +
+            'VALUES (?, ?, ?, ?, ?, ?)'
+        )
+        params = [
+            (dbRef, schemaName, table.name, column.name, str(column.type), int(1 if column.nullable else 0)),
+        ]
+        result = self.dbCursor.executemany(
+            insertSql,
+            params
+        )
+        self.dbConnection.commit()
+        
+    # end function
+    
+    def close(self):
+        logging.debug('Local database connection: closing ...')
+        try:
+            self.dbCursor.close()
+            self.dbConnection.close()
+            logging.debug('Local database connection: closed.')
+        except Exception as err:
+            logging.warning('Local database connection: error on close(): ')
+            print(err)
+        # end try catch
+    # end function
+# end class MyDbLocal
+
+class MyDb:
+    '''
+    To read database structure: tables and columns
+    '''
+    
+    # constructor
+    def __init__ (self, dbRef, dbLocal):
         self.dbRef        = dbRef
         logging.debug('DB: ' + self.dbRef + ' > new instance.')
         self.sshServer    = None
         self.dbEngine     = None
         self.dbConnection = None
         self.metadata     = None
+        self.dbLocal      = dbLocal
     # end constructor
     
+    # connect to SSH and DB
     def connect(self, config):
         logging.debug('DB: ' + self.dbRef + ' > connect: reading config ...')
         defaults  = config['defaults']
@@ -92,6 +216,7 @@ class MyDb:
         # end for
     # end function
 
+    # read tables
     def tables(self):
         logging.debug('DB: ' + self.dbRef + ' > metadata: loading ...')
         self.metadata = MetaData()
@@ -110,6 +235,7 @@ class MyDb:
         logging.debug('DB: ' + self.dbRef + ' > tables: listed.')
     # end function
 
+    # read table columns
     def table(self, myTable):
         logging.debug('DB: ' + self.dbRef + ' >> Table: ' + myTable.name + ' - Columns listing ...')
         #myTable = Table(myTable.name, self.metadata, autoload=True, autoload_with=self.dbEngine)
@@ -118,31 +244,29 @@ class MyDb:
                 'DB: ' + self.dbRef + ' >> Table: ' + myTable.name + ' Column: ' + myColumn.name +
                 ' Type: ' + str(myColumn.type) + ' Nullable: ' + str(myColumn.nullable)
             )
+            self.dbLocal.saveTableColumn(self.dbRef, 'public', myTable, myColumn) # schema = 'public'
         # end for
         logging.debug('DB: ' + self.dbRef + ' >> Table: ' + myTable.name + ' - Columns listed.')
-    # end function
-
-    def storeTableColumnInfo(self, table, column):
-        logging.debug('DB: ' + self.dbRef + ' > Store info ...')
     # end function
 
     def close(self):
         logging.debug('DB: ' + self.dbRef + ' > Database connection: closing ...')
         try:
             self.dbConnection.close()
+            logging.debug('DB: ' + self.dbRef + ' > Database connection: closed.')
         except Exception as err:
             logging.warning('DB: ' + self.dbRef + ' > Database connection: error on close(): ')
             print(err)
         # end try catch
-        logging.debug('DB: ' + self.dbRef + ' > Database connection: closed.')
 
         logging.debug('DB: ' + self.dbRef + ' > SSH tunnel: stopping ...')
         try:
             self.sshServer.stop()
+            logging.debug('DB: ' + self.dbRef + ' > SSH tunnel: stopped.')
         except Exception as err:
             logging.warning('DB: ' + self.dbRef + ' > SSH tunnel: error on close(): ')
             print(err)
-        logging.debug('DB: ' + self.dbRef + ' > SSH tunnel: stopped.')
+        # end try catch
     # end function
 
 # end class MyDb
@@ -152,6 +276,7 @@ class MyApp:
     def __init__ (self, configFilePath):
         logging.debug('MyApp: new instance.')
         self.read(configFilePath)
+        self.dbLocal = None
     # end constructor
     
     def read(self, configFilePath):
@@ -168,6 +293,10 @@ class MyApp:
 
     def start(self):
         logging.debug('MyApp: starting ...')
+        
+        self.dbLocal = MyDbLocal()
+        self.dbLocal.connect('./db.sqlite3');
+        
         compareDbs = self.config['compare']
         for key in compareDbs:
             sourceDbRef = key
@@ -175,7 +304,7 @@ class MyApp:
             logging.debug('MyApp: comparing: ' + sourceDbRef + ' with ' + targetDbRef)
             try:
                 logging.debug('MyApp: loading: ' + sourceDbRef)
-                sourceDb = MyDb(sourceDbRef)
+                sourceDb = MyDb(sourceDbRef, self.dbLocal)
                 logging.debug('MyApp: loaded: ' + sourceDbRef)
                 sourceDb.connect(self.config)
                 logging.debug('MyApp: connected: ' + sourceDbRef)
@@ -189,7 +318,7 @@ class MyApp:
 
             try:
                 logging.debug('MyApp: loading: ' + targetDbRef)
-                targetDb = MyDb(targetDbRef)
+                targetDb = MyDb(targetDbRef, self.dbLocal)
                 targetDb.connect(self.config)
                 targetTables = targetDb.tables()
             except Exception as err:
@@ -199,6 +328,8 @@ class MyApp:
                 targetDb.close()
             # end try catch
         # end for loop
+        
+        self.dbLocal.close()
     # end function
 
     def finish(self):
--- db.sql
+++ db.sql
@@ -1,15 +1,16 @@
 CREATE TABLE my_tables (
+  id              INTEGER PRIMARY KEY,
   db_ref          TEXT NOT NULL,
   schema_name     TEXT NOT NULL,
   table_name      TEXT NOT NULL,
   column_name     TEXT NOT NULL,
   column_type     TEXT NOT NULL,
   column_nullable INTEGER NOT NULL,
-  column_attrs    TEXT,
-  PRIMARY KEY (db_ref, schema_name, table_name, column_name)
+  column_attrs    TEXT
 );
 
 CREATE INDEX idx_db_ref      ON my_tables (db_ref);
 CREATE INDEX idx_schema_name ON my_tables (schema_name);
 CREATE INDEX idx_table_name  ON my_tables (table_name);
 CREATE INDEX idx_column_name ON my_tables (column_name);
+CREATE UNIQUE INDEX unq_db_schema_table_col ON my_tables (db_ref, schema_name, table_name, column_name);
